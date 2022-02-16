4909df390b7be06d80859a3417f932bd16af2ce0
Mor <mor.tabuch10@gmail.com>
2022-02-16 21:55:03 +0200

    h

--- config/mail.php
+++ config/mail.php
@@ -37,8 +37,8 @@ return [
         'smtp' => [
             'transport' => 'smtp',
             'host' =>  'smtp.gmail.com',
-            'port' => 465,
-            'encryption' => 'ssl',
+            'port' => 587,
+            'encryption' => 'tls',
             'username' => 'funny.stack1@gmail.com',
             'password' =>  '290508Mor',
             'timeout' => null,
